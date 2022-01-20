1ab874c82b1851f117e2b80ba9adb08544eba063
kuba <kuba@pop-os.localdomain>
2022-01-19 20:53:22 -0600

    added config to hide secrets

--- news_scanner/config.py
+++ news_scanner/config.py
@@ -3,29 +3,27 @@
 import os
 from news_scanner.twitter_handle.twitter_handle import TwitterHandleConfig
 from news_scanner.td_api.td_api_handle import TDAPIConfig
-import subprocess
 from pathlib import Path
+from dotenv import load_dotenv
 
-EXPORT_SECRETS_SCRIPT_PATH = Path(__file__).parent.parent / "export_secrets.sh"
-SECRETS_SCRIPT_NOT_FOUND_ERROR = "Secrets script not found at:"
+BASE_DIR = Path(__file__).parent.parent
+SECRETS_PATH = BASE_DIR / ".env"
+SECRETS_NOT_FOUND = "No .env file found at "
 
 
 class Config:
     """ Class to handle configuration.
 
-    This class reads secrets from environment variables set by a shell script.
-    The shell script should contain values for all variables being exported.
+    Reads secrets from .env file located in root project directory.
 
-    Note: May need to 'chmod +x <secrets_script path>'.
-
-    Ex in file ../News-Scrapper/export_secrets.sh:
-        export WEBSITE_URL="<website_url>"
-        export TWITTER_API_KEY="<twitter_api_key>"
-        export TWITTER_API_SECRET_KEY="<twitter_api_secret_key>"
-        export TWITTER_ACCESS_TOKEN="<twitter_access_token>"
-        export TWITTER_ACCESS_TOKEN_SECRET="<twitter_access_token_secret>"
-        export TDA_API_KEY="<tda_api_key>"
-        export TDA_REDIRECT_URI="<tda_redirect_uri>"
+    Ex News-Scrapper/.env file:
+        WEBSITE_URL="<website_url>"
+        TWITTER_API_KEY="<twitter_api_key>"
+        TWITTER_API_SECRET_KEY="<twitter_api_secret_key>"
+        TWITTER_ACCESS_TOKEN="<twitter_access_token>"
+        TWITTER_ACCESS_TOKEN_SECRET="<twitter_access_token_secret>"
+        TDA_API_KEY="<tda_api_key>"
+        TDA_REDIRECT_URI="<tda_redirect_uri>"
     """
     # env variable names
     _WEBSITE_URL = "WEBSITE_URL"
@@ -37,18 +35,13 @@ class Config:
     _TDA_REDIRECT_URI = "TDA_REDIRECT_URI"
     _TDA_TOKEN_PATH = "TDA_TOKEN_PATH"
 
-    def __init__(
-        self,
-        export_secrets_script_path: Path = EXPORT_SECRETS_SCRIPT_PATH
-    ):
-        """ Retrieves secrets and api keys from environment variables.
-
-        Params:
-            export_secrets_script_path: Path to script file exporting secrets.
-        """
-        _load_secrets(
-            export_secrets_script_path=export_secrets_script_path
-        )
+    def __init__(self):
+        """ Retrieves secrets and api keys from environment variables. """
+        # check if .env exists
+        if not SECRETS_PATH.is_file():
+            raise ValueError(SECRETS_NOT_FOUND+str(SECRETS_PATH))
+        # load env vars from .env
+        load_dotenv()
 
         self.website_url = os.environ[self._WEBSITE_URL]
         self.twitter_config = TwitterHandleConfig(
@@ -61,19 +54,3 @@ class Config:
             api_key=os.environ[self._TDA_API_KEY],
             redirect_uri=os.environ[self._TDA_REDIRECT_URI],
         )
-
-
-def _load_secrets(
-    export_secrets_script_path: Path = EXPORT_SECRETS_SCRIPT_PATH
-) -> None:
-    """ Loads secrets from script into environment.
-
-    Params:
-        export_secrets_script_path: Path to script file exporting secrets.
-    """
-    print("loading secrets")
-    if not export_secrets_script_path.is_file():
-        raise ValueError(
-            f"{SECRETS_SCRIPT_NOT_FOUND_ERROR} {export_secrets_script_path}"
-        )
-    subprocess.call(["sh", str(export_secrets_script_path)])
--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     packages=["news_scanner"],
     install_requires=[
         "tda-api", "pytest", "pandas", "bs4", "requests", "selenium",
-        "httpx", "twython", "numpy"
+        "httpx", "twython", "numpy", "python-dotenv"
     ]
 )
